c4ad16920bbc026ece25f6b4d2a9bf9b5252d188
Rob Campbell <git@raacampbell.com>
2015-12-28 16:50:10 +0100

    create an external function to import the structure list and nicely import all the rows (unlike before)

---
+++ getAllenStructureList.m
@@ -0,0 +1,81 @@
+function [ARA_list, col_names] = getAllenStructureList(downloadAgain)
+% Download the list of adult mouse structures from the Allen API. 
+%
+% function [ARA_list, col_names] = getAllenStructureList(downloadAgain)
+%
+% Purpose
+% Make an API query to read in the Allen Reference Atlas (ARA) brain area
+% list. All areas and data are read. Data are cached in the system's temp
+% directory and re-read from here if possible to improve speed. 
+% 
+% Inputs
+% If downloadAgain is supplied and is true, the function wipes cached data
+% and re-reads. zero by default. 
+%
+%
+% Outputs
+% ARA_list - a cell array containing the imported data. missing values replaced with -inf
+% col_names - the names of the columns
+%
+%
+%
+% Rob Campbell - Basel 2015
+
+
+if nargin<1
+	downloadAgain=0;
+end
+
+
+
+cachedCSV = sprintf('/tmp/%s_CACHED.csv',mfilename);
+cachedMAT =  sprintf('/tmp/%s_CACHED.mat',mfilename);
+
+
+
+if ~downloadAgain 
+	if exist(cachedMAT) %If the data have been imported before we can just return them
+		load(cachedMAT)
+		return
+	end
+end
+
+%If we get here, the user either asked for the data be re-read or we couldn't find any cached data
+
+
+
+% The adult mouse structure graph has an id of 1.		
+url='http://api.brain-map.org/api/v2/data/Structure/query.csv?criteria=[graph_id$eq1]&num_rows=all';
+[~,status] = urlwrite(url,cachedCSV);
+if ~status
+	error('Failed to get CSV file from URL %s', url)
+end
+
+
+fid = fopen(cachedCSV);
+if fid<0
+	error('Failed to open CSV file at %s\n', cachedCSV)
+end
+
+
+col_names = strsplit(fgetl(fid),','); %The names of the columns in the main cell array
+
+%Loop through and read each data row
+ARA_list={};
+tline = fgetl(fid);
+
+while ischar(tline)
+	%First we need to get rid of any 
+	tmp=textscan(tline,'%d%d%q%s%d%d%d%d%d%d%d%d%s%s%s%s%s%d%d%d%s','delimiter',',','emptyvalue',-inf);
+	ARA_list = [ARA_list;tmp];
+	tline=fgetl(fid);
+end
+
+
+
+
+
+fclose(fid);
+
+
+save(cachedMAT,'col_names','ARA_list')
--- structureID2name.m
+++ structureID2name.m
@@ -10,47 +10,27 @@ function [names,ARA_LIST]=structureID2name(structIDs,ARA_LIST)
 %
 % Inputs
 % structIDs - a vector (list) of integers corresponding to brain structure ids 
-% ARA_LIST - [optional] the result of reading the brain areas as a CSV file. 
-%			 e.g.
-%            url='http://api.brain-map.org/api/v2/data/Structure/query.csv?criteria=[graph_id$eq1]&num_rows=all'
-%            L=urlread(url);
-%            is missing, this is is read automatically and cached.
+% ARA_LIST - [optional] the first output of getAllenStructureList
 %
 %
 % Outputs
 % names - a list of brain area names
-% ARA_LIST - the raw CSV data
+% ARA_LIST - the CSV data in the form of a cell array
 %
 % Rob Campbell
-
+%
+% See also:
+% getAllenStructureList
 
 
 if nargin<2
-
-	cachedURL = '/tmp/cachedARA_LIST.csv';
-	cachedNameList = '/tmp/cachedARA_LIST.mat';
-	if ~exist(cachedURL,'file') %download and cache the JSON file
-		url='http://api.brain-map.org/api/v2/data/Structure/query.csv?criteria=[graph_id$eq1]&num_rows=all';
-		fprintf('Getting JSON from %s and caching to %s%s\n',url, fileparts(cachedURL),filesep)
-
-		[~,status] = urlwrite(url,cachedURL);
-		if ~status
-			error('Failed to get JSON from URL %s', url)
-		end
-		ARA_LIST = readCachedURL(cachedURL);
-		save(cachedNameList,'ARA_LIST')
-	else
-		load(cachedNameList);
-	end
-
-
-
+	ARA_LIST = getAllenStructureList;
 end
 
 
 %loop through and find all the IDs
 names={};
-allIDs = cellfun(@str2num,ARA_LIST(:,1));
+allIDs = [ARA_LIST{:,1}];
 
 
 for ii=1:length(structIDs)
@@ -61,30 +41,6 @@ for ii=1:length(structIDs)
 	if length(f)>1
 		error('found more than one ID index')
 	end
-	names{ii} = ARA_LIST{f,2};
+	names{ii} = ARA_LIST{f,3}{1};
 end
 
-
-
-
-
-%--------------------------------------------------
-function data = readCachedURL(fname)
-% read csv file containing data from web into a cell array 
-% this sub-function maybe should be moved to an stand-alone one at some point
-
-fid = fopen(fname);
-
-tline = fgetl(fid); %read and dump first line that contains the column headings
-
-data={};
-while ischar(tline)
-	tline=fgetl(fid);
-	if ischar(tline)
-		tmp=strsplit(tline,',');
-		data = [data;[tmp(1),tmp(end)]]; %keep only id, atlas id, and the structure name
-	end
-
-end
-
-fclose(fid);
