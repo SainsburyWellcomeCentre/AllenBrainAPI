52ea07253f471fe6c87cc0e54c5b84da13cafffb
Rob Campbell <git@raacampbell.com>
2015-12-28 14:22:50 +0100

    re-write the web query in structureID2name so it pulls in a CSV file and does not require the JSON reader. See: http://api.brain-map.org/examples/doc/thalamus/thalamus.R.html

--- structureID2name.m
+++ structureID2name.m
@@ -1,7 +1,7 @@
-function [names,ARA_JSON]=structureID2name(structIDs,ARA_JSON)
+function [names,ARA_LIST]=structureID2name(structIDs,ARA_LIST)
 % convert a list of ARA structure IDs to a cell array of names
 %
-% function [names,ARA_JSON]=structureID2name(structIDs,ARA_JSON)
+% function [names,ARA_LIST]=structureID2name(structIDs,ARA_LIST)
 %
 % Purpose
 % Each Allen Reference Atlas (ARA) brain area is associated with a unique
@@ -10,59 +10,49 @@ function [names,ARA_JSON]=structureID2name(structIDs,ARA_JSON)
 %
 % Inputs
 % structIDs - a vector (list) of integers corresponding to brain structure ids 
-% ARA_JSON - [optional] the ARA JSON imported as a structure. You may have a local 
-%            copy of this, in which case you could J = loadjson(PATH)
-%            if this is left bank, the JSON is pulled off the web and cached as 
-%            both a text file and a MATLAB structure.
+% ARA_LIST - [optional] the result of reading the brain areas as a CSV file. 
+%			 e.g.
+%            url='http://api.brain-map.org/api/v2/data/Structure/query.csv?criteria=[graph_id$eq1]&num_rows=all'
+%            L=urlread(url);
+%            is missing, this is is read automatically and cached.
 %
 %
 % Outputs
 % names - a list of brain area names
-% ARA_JSON - the JSON structure
+% ARA_LIST - the raw CSV data
 %
 % Rob Campbell
 
 
 
-if ~exist('loadjson')
-   disp('Please install JSONlab from the FEX') 
-   return
-end
-
-
-
 if nargin<2
-	cachedJSON = '/tmp/cachedARA_JSON.json';
-	cachedJSONstruct = '/tmp/cachedARA_JSON.mat';
 
-	if ~exist(cachedJSON,'file') %download and cache the JSON file
-		JSON_URL = 'http://api.brain-map.org/api/v2/structure_graph_download/1.json';
-		fprintf('Getting JSON from %s and caching to %s%s\n',JSON_URL, fileparts(cachedJSON),filesep)
+	cachedURL = '/tmp/cachedARA_LIST.csv';
+	cachedNameList = '/tmp/cachedARA_LIST.mat';
+	if ~exist(cachedURL,'file') %download and cache the JSON file
+		url='http://api.brain-map.org/api/v2/data/Structure/query.csv?criteria=[graph_id$eq1]&num_rows=all';
+		fprintf('Getting JSON from %s and caching to %s%s\n',url, fileparts(cachedURL),filesep)
 
-		[~,status] = urlwrite(JSON_URL,cachedJSON);
+		[~,status] = urlwrite(url,cachedURL);
 		if ~status
-			error('Failed to get JSON from URL %s', JSON_URL)
+			error('Failed to get JSON from URL %s', url)
 		end
-
-		ARA_JSON = loadjson(cachedJSON);
-	
-		save(cachedJSONstruct,'ARA_JSON')
+		ARA_LIST = readCachedURL(cachedURL);
+		save(cachedNameList,'ARA_LIST')
 	else
-		load(cachedJSONstruct)
+		load(cachedNameList);
 	end
+
+
+
 end
 
-
-
-
-
-%flatten the JSON and keep only the rows we care about
-flattened = looper(ARA_JSON.msg{1});
-
-allIDs = [flattened{:,1}];
 
 %loop through and find all the IDs
 names={};
+allIDs = cellfun(@str2num,ARA_LIST(:,1));
+
+
 for ii=1:length(structIDs)
 	f=find(allIDs == structIDs(ii));
 	if isempty(f)
@@ -71,25 +61,30 @@ for ii=1:length(structIDs)
 	if length(f)>1
 		error('found more than one ID index')
 	end
-	names{ii} = flattened{f,2};
+	names{ii} = ARA_LIST{f,2};
 end
 
 
 
 
-function out = looper(json,out)
-% looper searches recursively through the structure to build the flattened cell array
-if nargin<2
-	out{1,1} = json.id;
-	out{1,2} = json.name;
-	looper(json,out);
+
+%--------------------------------------------------
+function data = readCachedURL(fname)
+% read csv file containing data from web into a cell array 
+% this sub-function maybe should be moved to an stand-alone one at some point
+
+fid = fopen(fname);
+
+tline = fgetl(fid); %read and dump first line that contains the column headings
+
+data={};
+while ischar(tline)
+	tline=fgetl(fid);
+	if ischar(tline)
+		tmp=strsplit(tline,',');
+		data = [data;[tmp(1),tmp(end)]]; %keep only id, atlas id, and the structure name
+	end
+
 end
 
-for ii=1:length(json.children)
-	c = json.children{ii};
-	tmp{1,1} = c.id;
-	tmp{1,2} = c.name;
-	out = [out;tmp];
-	out = looper(c,out);
-end
-
+fclose(fid);
